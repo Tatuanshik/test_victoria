5f82723601c280d9b67df5373448fd506d9a9ba2
Таня Марковская <tanamarkovskaa@MacBook-Air-Tana.local>
2024-08-27 13:07:06 +0300

    third

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 from aiogram import Dispatcher, Bot
 import asyncio
 import logging
+import signal
+import sys
+import time
 #from config import BOT_TOKEN
 from handler import router
 import os
@@ -8,18 +11,27 @@ import json
 
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
-logging.basicConfig(level=logging.INFO)
-bot = Bot(token=BOT_TOKEN)
-dp = Dispatcher()
-dp.include_router(router)
+def signal_handler(signum, frame):
+    print("Получилм SIGTERM, выключение...")
+    sys.exit(0)
+
+    signal.signal(signal.SIGTERM, signal_handler)
 
 
-async def main():
-    logging.info("Бот запускается...")
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+while True:
+    time.sleep(1)
+    logging.basicConfig(level=logging.INFO)
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+    dp.include_router(router)
 
 
-if __name__ == '__main__':
-    asyncio.run(main())
+    async def main():
+        logging.info("Бот запускается...")
+        await bot.delete_webhook(drop_pending_updates=True)
+        await dp.start_polling(bot)
+
+
+    if __name__ == '__main__':
+        asyncio.run(main())
 
