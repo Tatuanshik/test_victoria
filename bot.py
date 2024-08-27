4d119db6c0336bfbcf5d0e842292762c846a60d0
Таня Марковская <tanamarkovskaa@MacBook-Air-Tana.local>
2024-08-27 16:17:15 +0300

    six

--- bot.py
+++ bot.py
@@ -4,10 +4,14 @@ import logging
 import signal
 import sys
 import os
-#from config import BOT_TOKEN
+import http.server
+import socketserver
+from config import BOT_TOKEN
+from threading import Thread
+from handler import router
 
 # Load the bot token from environment variables
-BOT_TOKEN = os.getenv('BOT_TOKEN')
+#BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -15,32 +19,34 @@ logging.basicConfig(level=logging.INFO)
 # Initialize bot and dispatcher
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
-
-# Import router after bot and dispatcher initialization
-from handler import router
 dp.include_router(router)
 
 # Signal handler function for graceful shutdown
 def signal_handler(signum, frame):
     logging.info("Received SIGTERM, shutting down...")
-    # Stop polling and close the bot
     asyncio.create_task(dp.storage.close())
     asyncio.create_task(dp.storage.wait_closed())
     sys.exit(0)
 
-# Register the signal handler
+
 signal.signal(signal.SIGTERM, signal_handler)
 
 
+def start_http_server():
+    port = int(os.getenv('PORT', 10000))
+    handler = http.server.SimpleHTTPRequestHandler
+    with socketserver.TCPServer(("0.0.0.0", port), handler) as httpd:
+        logging.info(f"Serving HTTP on port {port}")
+        httpd.serve_forever()
+        
 async def main():
     logging.info("Starting bot...")
     await bot.delete_webhook(drop_pending_updates=True)
     try:
-        # Start polling
         await dp.start_polling(bot)
     except (KeyboardInterrupt, SystemExit):
-        # Handle shutdown on interrupt
         logging.info("Shutting down...")
 
 if __name__ == '__main__':
+    Thread(target=start_http_server).start()
     asyncio.run(main())
